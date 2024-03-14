945ba5b3035af5844d3a1a69dbe4a1d68eb5cb80
SabbyCodingEmpress <sabina_guerrero@yahoo.com>
2024-03-13 21:52:30 -0700

    updated

---
+++ src/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+/** base url to make requests to the movie database */
+
+const instance = axios.create({
+    baseUrl: "https://api.themoviedb.org/3",
+});
+
+export default instance;
